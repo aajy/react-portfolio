99467a1dfe5e399dcbe77997f06ccea5189bf843
aajy <aajy.000@gmail.com>
2023-11-24 11:49:49 +0900

    react-icon 추가,footer 스타일링

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,5 +1,22 @@
 import './Footer.scss';
+import { FaFacebookF, FaTwitter, FaInstagramSquare } from 'react-icons/fa';
 
 export default function Footer() {
-	return <div className='Footer'>Footer</div>;
+	return (
+		<footer className='Footer'>
+			<h1>Dcodelab</h1>
+			<p>2023 DcodeLab &copy; All Rights Reserved. </p>
+			<ul>
+				<li>
+					<FaFacebookF />
+				</li>
+				<li>
+					<FaTwitter />
+				</li>
+				<li>
+					<FaInstagramSquare />
+				</li>
+			</ul>
+		</footer>
+	);
 }
