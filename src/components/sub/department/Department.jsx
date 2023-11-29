9bc5104860528b8496fe1c95e7de6e8f3197588e
aajy <aajy.000@gmail.com>
2023-11-29 11:27:15 +0900

    department 페이지 history section 추가,scss변수 tablet추가

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,45 +1,62 @@
 import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Department.scss';
-import { useCombinedText, useCustomText } from '../../../hooks/useText';
+import { useCustomText } from '../../../hooks/useText';
 
 export default function Department() {
+	const [HistoryTit, setHistoryTit] = useState('');
+	const [HistoryData, setHistoryData] = useState([]);
 	const [MemberTit, setMemberTit] = useState('');
 	const [MemberData, setMemberData] = useState([]);
-	const [MemberName, setMemberName] = useState([]);
-	// const splitText = useSplitText();
-	// const splitRefs = MemberName.map(useRef());
 
 	const path = useRef(process.env.PUBLIC_URL); //public 폴더까지의 경로를구하는 구문
-	const changeTitle = useCustomText('title');
 	const shortenText = useCustomText('shorten');
 	const combinedTitle = useCustomText('combined');
-	const test1 = 'our-members-score';
-	console.log(combinedTitle(test1));
+
 	const fetchDepartmemt = () => {
 		fetch(`${path.current}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
 				setMemberTit(Object.keys(json)[0]); //객체를 반복돌며 key값만 배열로 반환
 				setMemberData(Object.values(json)[0]); //객체를 반복돌며 value값만 배열로 반환
-				setMemberName(Object.values(json)[0].map((el) => el.name));
+				console.log(
+					'nameArr',
+					Object.values(json)[0].map((el) => el.name)
+				);
+			});
+	};
+	const fetchHistory = () => {
+		fetch(`${path.current}/DB/history.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				setHistoryTit(Object.keys(json)[0]);
+				setHistoryData(Object.values(json)[0]);
 			});
 	};
 	useEffect(() => {
 		fetchDepartmemt();
-		// setTimeout(() => {
-		// 	MemberName.forEach((name, idx) => {
-		// 		const ref = splitRefs[idx].current;
-		// 		if (ref) {
-		// 			splitText(ref, name);
-		// 		}
-		// 		// splitText(splitRefs[idx].current, name);
-		// 	});
-		// }, 300);
+		fetchHistory();
 	}, []);
 
 	return (
 		<Layout title={'Department'}>
+			<div className='historyBox'>
+				<h2>{combinedTitle(HistoryTit)}</h2>
+				<div className='con'>
+					{HistoryData.map((history, idx) => {
+						return (
+							<article key={history + idx}>
+								<h3>{Object.keys(history)[0]}</h3>
+								<ul>
+									{Object.values(history)[0].map((txt, idx) => {
+										return <li key={txt + idx}>{txt}</li>;
+									})}
+								</ul>
+							</article>
+						);
+					})}
+				</div>
+			</div>
 			<section className='memberBox'>
 				<h2>{combinedTitle(MemberTit)}</h2>
 				<div className='con'>
@@ -59,30 +76,6 @@ export default function Department() {
 					})}
 				</div>
 			</section>
-			<div className='benefits'>
-				<h2>{`${MemberTit.charAt(0).toUpperCase() + MemberTit.slice(1)}`}</h2>
-				<article>
-					{MemberData.map((member, idx) => {
-						return (
-							<span key={member + idx}>
-								<em></em>
-								<h3>{member.position}</h3>
-								<span>
-									{member.name} : Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Ratione sunt fugit quidem.
-								</span>
-							</span>
-						);
-					})}
-				</article>
-			</div>
-			{/* <div className='splitName'>
-				{MemberName.map((name, idx) => (
-					<p key={idx} ref={splitRefs[idx]}>
-						{name}
-					</p>
-				))}
-			</div> */}
 		</Layout>
 	);
 }
