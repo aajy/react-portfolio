78d2bb044dbfe9806851a92aae1c2859f99e47f0
aajy <aajy.000@gmail.com>
2023-11-30 13:21:44 +0900

    옵셔널체이닝을 && 연산자로 변경

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -24,15 +24,19 @@ export default function Detail() {
 	}, []);
 	return (
 		<Layout title={'Detail'}>
-			<div className='videoBox'>
-				<iframe
-					title={YoutubeData?.title}
-					src={`https://www.youtube.com/embed/${YoutubeData?.resourceId.videoId}`}
-					frameborder='0'
-				></iframe>
-			</div>
-			<h3>{YoutubeData?.title}</h3>
-			<p>{YoutubeData?.description}</p>
+			{YoutubeData && (
+				<article>
+					<div className='videoBox'>
+						<iframe
+							title={YoutubeData.title}
+							src={`https://www.youtube.com/embed/${YoutubeData.resourceId.videoId}`}
+							frameborder='0'
+						></iframe>
+					</div>
+					<h3>{YoutubeData.title}</h3>
+					<p>{YoutubeData.description}</p>
+				</article>
+			)}
 		</Layout>
 	);
 }
