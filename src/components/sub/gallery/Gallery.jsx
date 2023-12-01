09dc1cb5a099ee863e4859bb1502172fa94fe980
aajy <aajy.000@gmail.com>
2023-12-01 12:44:56 +0900

    masonry 설치, 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
+import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
 	const fetchFlickr = async () => {
-		const num = 100;
+		const num = 10;
 		const flickr_api = process.env.REACT_APP_FLICKR_API;
 		const method_interest = 'flickr.interestingness.getList';
 		const baseURL = 'https://www.flickr.com/services/rest/?method=';
@@ -14,40 +15,53 @@ export default function Gallery() {
 		const json = await data.json();
 		setPics(json.photos.photo);
 	};
+	const masonryOptions = {
+		transitionDuration: 0,
+	};
+
+	const imagesLoadedOptions = { background: '.my-bg-image-el' };
 	useEffect(() => {
 		fetchFlickr();
 	}, []);
 	return (
 		<Layout title={'Gallery'}>
-			<div className='frame'>
-				{Pics.map((pic, idx) => {
-					return (
-						<article key={pic.id}>
-							<div className='pic'>
-								<img
-									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-								/>
-							</div>
-							<h2>{pic.title}</h2>
+			<section>
+				<Masonry
+					className={'frame'}
+					elementType={'div'}
+					options={{ transitionDuration: 0.5 }}
+					disableImagesLoaded={false}
+					updateOnEachImageLoad={false}
+				>
+					{Pics.map((pic, idx) => {
+						return (
+							<article key={pic.id}>
+								<div className='pic'>
+									<img
+										src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+									/>
+								</div>
+								<h2>{pic.title}</h2>
 
-							<div className='profile'>
-								<img
-									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-									alt='사용자 프로필 이미지'
-									onError={(e) =>
-										e.target.setAttribute(
-											'src',
-											'https://www.flickr.com/images/buddyicon.gif'
-										)
-									}
-								/>
-								<span>{pic.owner}</span>
-							</div>
-						</article>
-					);
-				})}
-			</div>
+								<div className='profile'>
+									<img
+										src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+										alt='사용자 프로필 이미지'
+										onError={(e) =>
+											e.target.setAttribute(
+												'src',
+												'https://www.flickr.com/images/buddyicon.gif'
+											)
+										}
+									/>
+									<span>{pic.owner}</span>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
+			</section>
 		</Layout>
 	);
 }
