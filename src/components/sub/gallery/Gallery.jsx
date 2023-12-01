28294b5ea9815b7223cc7f1b029d63ccf7a00a62
aajy <aajy.000@gmail.com>
2023-12-01 11:40:06 +0900

    Gallery 페이지 flickr 이미지 불러오기, 환경변수에 flickr_api 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
+	const [Pics, setPics] = useState([]);
+	const fetchFlickr = async () => {
+		const num = 5;
+		const flickr_api = process.env.REACT_APP_FLICKR_API;
+		const method_interest = 'flickr.interestingness.getList';
+		const baseURL = 'https://www.flickr.com/services/rest/?method=';
+		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+		const data = await fetch(resultURL);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
+	useEffect(() => {
+		fetchFlickr();
+	}, []);
 	return (
 		<Layout title={'Gallery'}>
-			<p>Gallery전용 컨텐츠</p>
+			{Pics.map((pic, idx) => {
+				return (
+					<article key={pic.id}>
+						<img
+							src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+							alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+						/>
+						<h2>{pic.title}</h2>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
