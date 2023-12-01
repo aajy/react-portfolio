24fec8e4262aa46677471e178cca618b14364b66
aajy <aajy.000@gmail.com>
2023-12-01 12:09:02 +0900

    Gallery 페이지 이미지 업로더 프로필 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,14 +5,13 @@ import './Gallery.scss';
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
 	const fetchFlickr = async () => {
-		const num = 5;
+		const num = 100;
 		const flickr_api = process.env.REACT_APP_FLICKR_API;
 		const method_interest = 'flickr.interestingness.getList';
 		const baseURL = 'https://www.flickr.com/services/rest/?method=';
 		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
 		const data = await fetch(resultURL);
 		const json = await data.json();
-		console.log(json.photos.photo);
 		setPics(json.photos.photo);
 	};
 	useEffect(() => {
@@ -20,17 +19,35 @@ export default function Gallery() {
 	}, []);
 	return (
 		<Layout title={'Gallery'}>
-			{Pics.map((pic, idx) => {
-				return (
-					<article key={pic.id}>
-						<img
-							src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-							alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-						/>
-						<h2>{pic.title}</h2>
-					</article>
-				);
-			})}
+			<div className='frame'>
+				{Pics.map((pic, idx) => {
+					return (
+						<article key={pic.id}>
+							<div className='pic'>
+								<img
+									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+								/>
+							</div>
+							<h2>{pic.title}</h2>
+
+							<div className='profile'>
+								<img
+									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+									alt='사용자 프로필 이미지'
+									onError={(e) =>
+										e.target.setAttribute(
+											'src',
+											'https://www.flickr.com/images/buddyicon.gif'
+										)
+									}
+								/>
+								<span>{pic.owner}</span>
+							</div>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
