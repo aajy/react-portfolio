71fdafd0f16b39ccb422377e1f6312085acdc849
aajy <aajy.000@gmail.com>
2023-12-04 10:14:24 +0900

    Gallery 에서 유저 id를 이용해 이미지 불러오기

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,66 +1,65 @@
 import { useEffect, useState } from 'react';
+import Masonry from 'react-masonry-component';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
-import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
+
 	const fetchFlickr = async () => {
-		const num = 10;
+		const num = 50;
+		const myID = '198783018@N02';
 		const flickr_api = process.env.REACT_APP_FLICKR_API;
+		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
 		const method_interest = 'flickr.interestingness.getList';
-		const baseURL = 'https://www.flickr.com/services/rest/?method=';
-		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
-		const data = await fetch(resultURL);
+		const method_user = 'flickr.people.getPhotos';
+		const interestURL = `${baseURL}${method_interest}`;
+		const userURL = `${baseURL}${method_user}&user_id=${myID}`;
+
+		const data = await fetch(userURL);
 		const json = await data.json();
 		setPics(json.photos.photo);
 	};
-	const masonryOptions = {
-		transitionDuration: 0,
-	};
 
-	const imagesLoadedOptions = { background: '.my-bg-image-el' };
 	useEffect(() => {
 		fetchFlickr();
 	}, []);
+
 	return (
 		<Layout title={'Gallery'}>
-			<section className='frame'>
-				{/* <Masonry
+			<section>
+				<Masonry
 					className={'frame'}
-					elementType={'div'}
-					options={{ transitionDuration: 0.5 }}
-					disableImagesLoaded={false}
-					updateOnEachImageLoad={false}
-				> */}
-				{Pics.map((pic, idx) => {
-					return (
-						<article key={pic.id}>
-							<div className='pic'>
-								<img
-									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-								/>
-							</div>
-							<h2>{pic.title}</h2>
+					options={{ transitionDuration: '0.5s', gutter: 20 }}
+				>
+					{Pics.map((pic, idx) => {
+						return (
+							<article key={pic.id}>
+								<div className='pic'>
+									<img
+										src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+									/>
+								</div>
+								<h2>{pic.title}</h2>
 
-							<div className='profile'>
-								<img
-									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-									alt='사용자 프로필 이미지'
-									onError={(e) =>
-										e.target.setAttribute(
-											'src',
-											'https://www.flickr.com/images/buddyicon.gif'
-										)
-									}
-								/>
-								<span>{pic.owner}</span>
-							</div>
-						</article>
-					);
-				})}
-				{/* </Masonry> */}
+								<div className='profile'>
+									<img
+										src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+										alt='사용자 프로필 이미지'
+										onError={(e) =>
+											e.target.setAttribute(
+												'src',
+												'https://www.flickr.com/images/buddyicon.gif'
+											)
+										}
+									/>
+									<span>{pic.owner}</span>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
 			</section>
 		</Layout>
 	);
