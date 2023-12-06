c8da0730d5d992e6632b16da9a359d0d8edf1e3a
aajy <aajy.000@gmail.com>
2023-12-06 10:31:44 +0900

    Gallery페이지 masonry frame을 참조객체에 등록,사이 간격을 gap useRef 참조객체로 주기

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -9,6 +9,8 @@ export default function Gallery() {
 	const myID = useRef('198783018@N02');
 	const isUser = useRef(myID.current);
 	const refNav = useRef(null);
+	const refFrameWrap = useRef(null);
+	const gap = useRef(20);
 	const [Pics, setPics] = useState([]);
 	const [Open, setOpen] = useState(false);
 	const [Index, setIndex] = useState(0);
@@ -72,6 +74,7 @@ export default function Gallery() {
 	};
 
 	useEffect(() => {
+		refFrameWrap.current.style.setProperty('--gap', gap.current + 'px');
 		fetchFlickr({ type: 'user', id: myID.current });
 	}, []);
 
@@ -93,10 +96,10 @@ export default function Gallery() {
 					</form>
 				</article>
 
-				<section>
+				<section className='frameWrap' ref={refFrameWrap}>
 					<Masonry
 						className={'frame'}
-						options={{ transitionDuration: '0.5s', gutter: 20 }}
+						options={{ transitionDuration: '0.5s', gutter: gap.current }}
 					>
 						{Pics.length === 0 ? (
 							<h2>해당 키워드 검색결과가 없습니다.</h2>
