d562d346e169023db0ab3f67894941aca2dcadf9
aajy <aajy.000@gmail.com>
2023-12-11 12:36:40 +0900

    지도, 로드 뷰 토글버튼으로 전환

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -8,10 +8,13 @@ export default function Contact() {
 	//화면에 출력될 지도정보 배열의 순번이 담길 state
 	const [Index, setIndex] = useState(0);
 	const [Traffic, setTraffic] = useState(false);
+	const [View, setView] = useState(false);
+
 	const mapFrame = useRef(null);
+	const viewFrame = useRef(null);
+
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
-	const viewFrame = useRef(null);
 
 	//지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
 	const mapInfo = useRef([
@@ -59,20 +62,39 @@ export default function Contact() {
 
 	//컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
+		mapFrame.current.innerHTML = '';
 		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3,
 		});
 		marker.current.setMap(mapInstance.current);
 		setTraffic(false);
+
+		new kakao.current.maps.RoadviewClient().getNearestPanoId(
+			mapInfo.current[Index].latlng,
+			50,
+			(panoId) => {
+				new kakao.current.maps.Roadview(viewFrame.current).setPanoId(
+					panoId,
+					mapInfo.current[Index].latlng
+				);
+			}
+		);
+
+		//지도 타입 컨트롤러 추가
 		mapInstance.current.addControl(
 			new kakao.current.maps.MapTypeControl(),
 			kakao.current.maps.ControlPosition.TOPRIGHT
 		);
+
+		//지도 줌 컨트롤러 추가
 		mapInstance.current.addControl(
 			new kakao.current.maps.ZoomControl(),
 			kakao.current.maps.ControlPosition.RIGHT
 		);
+
+		//휠에 맵 줌 기능 비활성화
+		mapInstance.current.setZoomable(false);
 
 		window.addEventListener('resize', setCenter);
 		return () => window.removeEventListener('resize', setCenter);
@@ -87,6 +109,7 @@ export default function Contact() {
 					kakao.current.maps.MapTypeId.TRAFFIC
 			  );
 	}, [Traffic]);
+
 	return (
 		<Layout title={'Contact'}>
 			<div className='controlBox'>
@@ -101,18 +124,32 @@ export default function Contact() {
 						</button>
 					))}
 				</nav>
+
 				<nav className='info'>
-					<button
-						onClick={() => {
-							setTraffic(!Traffic);
-						}}
-					>
+					<button onClick={() => setTraffic(!Traffic)}>
 						{Traffic ? 'Traffic OFF' : 'Traffic ON'}
+					</button>
+					<button onClick={() => setView(!View)}>
+						{View ? 'map' : 'road view'}
 					</button>
 				</nav>
 			</div>
-			<article className='mapBox' ref={mapFrame}></article>
-			<article className='viewBox' ref={viewFrame}></article>
+			<section className='tab'>
+				<article
+					className={`mapBox ${View ? '' : 'on'}`}
+					ref={mapFrame}
+				></article>
+				<article
+					className={`viewBox ${View ? 'on' : ''}`}
+					ref={viewFrame}
+				></article>
+			</section>
 		</Layout>
 	);
 }
+
+/*
+	1.cdn불러온 window에 불러온 외부 객체값을 가져와서 인스턴스 생성
+	2.인스턴스값을 참조객체 담는 이유 (의존성배열에 불필요하게 등록하지 않기 위해서)
+	3.화면변경점이 발생해야 될떄 state값에 따라서 변경되게 로직화한 다음 이벤트 발생시 state를 변경해서 화면 재랜더링 유도
+*/
