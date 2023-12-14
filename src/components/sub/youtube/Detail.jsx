fbb9d644aefb52114b9f82b087226eea192a87b8
aajy <aajy.000@gmail.com>
2023-12-14 11:34:13 +0900

    redux, react-redux설치 및 폴더,파일생성

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -1,33 +1,29 @@
 import { useParams } from 'react-router-dom';
 import Layout from '../../common/layout/Layout';
 import './Detail.scss';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Detail() {
 	const { id } = useParams();
 	const [YoutubeData, setYoutubeData] = useState(null);
 
-	const fetchSingledata = async () => {
+	const fetchSingledata = useCallback(async () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
 
 		const data = await fetch(baseURL);
 		const json = await data.json();
 		setYoutubeData(json.items[0].snippet);
-	};
+	}, []);
 	useEffect(() => {
 		fetchSingledata();
-	}, []);
+	}, [fetchSingledata]);
 	return (
 		<Layout title={'Detail'}>
 			{YoutubeData && (
 				<article>
 					<div className='videoBox'>
-						<iframe
-							title={YoutubeData.title}
-							src={`https://www.youtube.com/embed/${YoutubeData.resourceId.videoId}`}
-							frameborder='0'
-						></iframe>
+						<iframe title={YoutubeData.title} src={`https://www.youtube.com/embed/${YoutubeData.resourceId.videoId}`} frameborder='0'></iframe>
 					</div>
 					<h3>{YoutubeData.title}</h3>
 					<p>{YoutubeData.description}</p>
