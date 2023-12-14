416992e5b8e6d423a2c92df2c1be30a1e7282f48
aajy <aajy.000@gmail.com>
2023-12-14 12:48:30 +0900

    전역 데이터 useSelctor로 원하는 컴포넌트 어디서든 자유롭게 호출

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,11 +1,19 @@
 import './Footer.scss';
 import { FaFacebookF, FaTwitter, FaInstagramSquare } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Footer() {
+	const MemberData = useSelector(store => store.memberReducer.members);
 	return (
 		<footer className='Footer'>
 			<h1>Dcodelab</h1>
 			<p>2023 DcodeLab &copy; All Rights Reserved. </p>
+			<div>
+				President :{' '}
+				{MemberData.map(member => {
+					if (member.position === 'President') return member.name;
+				})}
+			</div>
 			<ul>
 				<li>
 					<FaFacebookF />
--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -2,35 +2,37 @@ import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Department.scss';
 import { useCustomText } from '../../../hooks/useText';
+import { useSelector } from 'react-redux';
 
 export default function Department() {
+	const MemberData = useSelector(store => store.memberReducer.members);
+	const combinedTitle = useCustomText('combined');
 	const [HistoryTit, setHistoryTit] = useState('');
 	const [HistoryData, setHistoryData] = useState([]);
-	const [MemberTit, setMemberTit] = useState('');
-	const [MemberData, setMemberData] = useState([]);
+	// const [MemberTit, setMemberTit] = useState('');
+	// const [MemberData, setMemberData] = useState([]);
 
 	const path = useRef(process.env.PUBLIC_URL); //public 폴더까지의 경로를구하는 구문
 	const shortenText = useCustomText('shorten');
-	const combinedTitle = useCustomText('combined');
 
-	const fetchDepartmemt = () => {
-		fetch(`${path.current}/DB/department.json`)
-			.then((data) => data.json())
-			.then((json) => {
-				setMemberTit(Object.keys(json)[0]); //객체를 반복돌며 key값만 배열로 반환
-				setMemberData(Object.values(json)[0]); //객체를 반복돌며 value값만 배열로 반환
-			});
-	};
+	// const fetchDepartmemt = () => {
+	// 	fetch(`${path.current}/DB/department.json`)
+	// 		.then((data) => data.json())
+	// 		.then((json) => {
+	// 			setMemberTit(Object.keys(json)[0]); //객체를 반복돌며 key값만 배열로 반환
+	// 			setMemberData(Object.values(json)[0]); //객체를 반복돌며 value값만 배열로 반환
+	// 		});
+	// };
 	const fetchHistory = () => {
 		fetch(`${path.current}/DB/history.json`)
-			.then((data) => data.json())
-			.then((json) => {
+			.then(data => data.json())
+			.then(json => {
 				setHistoryTit(Object.keys(json)[0]);
 				setHistoryData(Object.values(json)[0]);
 			});
 	};
 	useEffect(() => {
-		fetchDepartmemt();
+		// fetchDepartmemt();
 		fetchHistory();
 	}, []);
 
@@ -54,16 +56,14 @@ export default function Department() {
 				</div>
 			</div>
 			<section className='memberBox'>
-				<h2>{combinedTitle(MemberTit)}</h2>
+				{/* <h2>{combinedTitle(MemberTit)}</h2> */}
+				<h2>{combinedTitle('Members')}</h2>
 				<div className='con'>
 					{MemberData.map((member, idx) => {
 						return (
 							<article key={member + idx}>
 								<div className='pic'>
-									<img
-										src={`${path.current}/img/${member.pic}`}
-										alt={member.name}
-									/>
+									<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
 									<h3>{member.name}</h3>
 									<p>{shortenText(member.position, 3)}</p>
 								</div>
