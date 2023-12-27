b286b93205ea765547e9c1678df6c9e25c79b8dd
aajy <aajy.000@gmail.com>
2023-12-27 12:54:44 +0900

    useQuery 옵션 retry추가, useHistoryQuery.js생성

--- src/hooks/useHistoryQuery.js
+++ src/hooks/useHistoryQuery.js
@@ -1,18 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 
 const path = process.env.PUBLIC_URL;
-const fetchDepartment = async () => {
-	const response = await fetch(`${path}/DB/department.json`);
+const fetchHihstory = async () => {
+	const response = await fetch(`${path}/DB/hihstory.json`);
 	const data = await response.json();
 	return data.members;
 };
 
-export const useDepartmentQuery = () => {
-	return useQuery(['fetchDepartment'], fetchDepartment, {
+export const useHihstoryQuery = () => {
+	return useQuery(['fetchHihstory'], fetchHihstory, {
 		refetchOnMount: false,
 		refetchOnWindowFocus: false,
 		cacheTime: 1000 * 60 * 60 * 24,
 		staleTime: 1000 * 60 * 60 * 24,
-		retry: 3 //데이터 요청 실패 시 재시도 횟수. default:3
+		retry: 3
 	});
 };
