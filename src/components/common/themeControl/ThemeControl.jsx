6dc2cf4380b284e1fc28b77f1e1c32615632b861
aajy <aajy.000@gmail.com>
2023-12-28 15:03:39 +0900

    테마 색상 변경 시 쿠키에 등록하고, 쿠키가 사라지기 전까지는쿠키값 반영

--- src/components/common/themeControl/ThemeControl.jsx
+++ src/components/common/themeControl/ThemeControl.jsx
@@ -4,7 +4,15 @@ import './ThemeControl.scss';
 
 export default function ThemeControl() {
 	const inputEl = useRef(null);
-	const { setCookie } = useCookie();
+	const { setCookie, isCookie } = useCookie();
+
+	//컴포넌트 마운트시 theme라는 이름의 쿠키값이 있으면
+	if (isCookie('theme')) {
+		//현재 쿠키값에서 'theme='다음 문자값을 가져와서 ;기준으로 배열분리한뒤 제일 첫번째값이 theme 컬러값
+		//해당 쿠키 컬러값으로 자동 세팅
+		document.body.style.setProperty('--pointColor', document.cookie.split('theme=')[1].split(';')[0]);
+	}
+	//만약 쿠키가 없으면 그냥 css에 등록되어 있는 기본 --pointColor값 활용
 
 	const changeThemeColor = () => {
 		const color = inputEl.current.value;
