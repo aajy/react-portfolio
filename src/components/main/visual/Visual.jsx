8edc36ee054877111489171bd1fe73dee29ab51d
aajy <aajy.000@gmail.com>
2023-12-29 10:26:35 +0900

    메인페이지 비주얼에 유튜브 전역데이터미리보기 출력

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,19 @@
 import './Visual.scss';
+import { useSelector } from 'react-redux';
 
 export default function Visual() {
-	return <div className='Visual'>Visual</div>;
+	const youtube = useSelector(store => store.youtubeReducer.youtube);
+	console.log('youtube', youtube);
+	return (
+		<figure className='Visual'>
+			{youtube.map((vid, idx) => {
+				if (idx >= 5) return null;
+				return (
+					<article key={vid.id}>
+						<h3>{vid.snippet.title}</h3>
+					</article>
+				);
+			})}
+		</figure>
+	);
 }
