7f483ca6bcb25a2161079c9077cf181437f62d89
aajy <aajy.000@gmail.com>
2023-12-29 12:24:13 +0900

    core 기능을 useSwipter hook을 이용해서 리액트에 자동롤링 기능,제어버튼추가 롤링 시작버튼 이벤트 시 다음슬라이드로 넘기면서 시작 loop사용 시 처음 마운트 시 마지막 패널이 보이는 이슈해결을 위해 강제로 다음 슬라이드 넘기기 추가

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,22 +1,51 @@
 import './Visual.scss';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+function Btns() {
+	//Swiper컴포넌트 안쪽에 있는 또다른 자식 컴포넌트 안쪽에서만 useSwiper hook사용가능
+	//hook으로부터 생성된 객체(인스턴스)에는 다양한 prototype메서드와 property값 활용가능
+	const swiper = useSwiper();
+
+	useEffect(() => {
+		swiper.slideNext(300);
+	}, [swiper]);
+
+	return (
+		<nav className='swiperController'>
+			<button
+				onClick={() => {
+					swiper.slideNext(300);
+					swiper.autoplay.start();
+				}}
+			>
+				start
+			</button>
+			<button onClick={() => swiper.autoplay.stop()}>stop</button>
+		</nav>
+	);
+}
 
 export default function Visual() {
-	const youtube = useSelector(store => store.youtubeReducer.youtube);
-	console.log('youtube', youtube);
+	const { youtube } = useSelector(store => store.youtubeReducer);
+
 	return (
 		<figure className='Visual'>
 			<Swiper
-				modules={[Pagination]}
+				modules={[Pagination, Autoplay]}
 				pagination={{
 					clickable: true,
 					renderBullet: (index, className) => {
 						return `<span class=${className}>${index + 1}</span>`;
 					}
+				}}
+				autoplay={{
+					delay: 5000,
+					disableOnInteraction: true
 				}}
 				loop={true}
 			>
@@ -25,11 +54,13 @@ export default function Visual() {
 					return (
 						<SwiperSlide key={vid.id}>
 							<div className='inner'>
-								<h3>{vid.snippet.title}</h3>
+								<h3>{idx + 1 + '. ' + vid.snippet.title}</h3>
 							</div>
 						</SwiperSlide>
 					);
 				})}
+
+				<Btns />
 			</Swiper>
 		</figure>
 	);
