3c212f4c7fffe2893ce480a3bf92f64530c67edd
aajy <aajy.000@gmail.com>
2024-01-03 11:46:00 +0900

    Btns컴포넌트 swipterRef가 의존성배열에 있는 useEffect 이벤트click문으로 수정

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -78,7 +78,7 @@ function Btns({ swiperRef }) {
 	//Btns컴포넌트에서 인스턴스의 이벤트문을 활용해서
 	useEffect(() => {
 		//slide가 바뀔때마다 현재 롤링유무에 따라 Rolling state값 변경
-		swiperRef.current.on('slideChange', () => {
+		swiperRef.current.on('click', () => {
 			swiperRef.current.autoplay.running ? setRolling(true) : setRolling(false);
 		});
 	}, [swiperRef]);
