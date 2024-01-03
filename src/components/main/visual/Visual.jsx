03def1e91453c1bb51401e1bea4fbfc6dcdf4ba4
aajy <aajy.000@gmail.com>
2024-01-03 15:15:26 +0900

    Visual - swiper text, pic영역으로 분리 및  스타일링

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -9,22 +9,18 @@ export default function Visual() {
 	const swiperOpt = useRef({
 		loop: true,
 		slidesPerView: 1,
-		spaceBetween: 0,
+		spaceBetween: 50,
 		centeredSlides: true,
+		onSwiper: swiper => swiper.slideNext(300),
 		breakpoints: {
-			1000: {
-				sliderPerView: 2,
-				spaceBetween: 50
-			},
-			1400: {
-				sliderPerView: 3,
-				spaceBetween: 50
-			}
+			1000: { slidesPerView: 2 },
+			1400: { slidesPerView: 3 }
 		}
 	});
 	console.log(data);
 	return (
 		<figure className='Visual'>
+			<div className='txtBox'></div>
 			<Swiper {...swiperOpt.current}>
 				{isSuccess &&
 					data.map((el, idx) => {
