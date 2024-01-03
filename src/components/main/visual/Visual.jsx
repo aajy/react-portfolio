72bac160f6011be3167b9e13f58a5417e620ed89
aajy <aajy.000@gmail.com>
2024-01-03 12:51:22 +0900

    Main에 youtube데이터 다중패널 swiper추가 - swiper컴포넌트 활성화 이벤트(OnSwiper)를 통해서 인스턴스 활용

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,40 @@
 import './Visual.scss';
+import { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import 'swiper/css';
 
 export default function Visual() {
-	return <div className='Visual'>Visual</div>;
+	const Vids = useSelector(store => store.youtube.data);
+	const swiperOpt = useRef({
+		modules: [Autoplay],
+		autoplay: { delay: 4000, disableOnInteraction: true },
+		spaceBetween: 70,
+		loop: true,
+		slidesPerView: 3,
+		centeredSlides: true,
+		onSwiper: swiper => {
+			swiper.slideNext(300);
+		}
+	});
+
+	return (
+		<figure className='Visual'>
+			<Swiper {...swiperOpt.current}>
+				{Vids.map((data, idx) => {
+					if (idx >= 5) return null;
+					return (
+						<SwiperSlide key={data.id}>
+							<div className='inner'>
+								<div className='picBox'>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</div>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</figure>
+	);
 }
