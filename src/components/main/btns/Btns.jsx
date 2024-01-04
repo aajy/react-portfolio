70f748d51ff54eb91a879e8f1f56c2aaa39f5724
aajy <aajy.000@gmail.com>
2024-01-04 15:40:16 +0900

    스크롤 wheel이벤트로 autoscroll 기능 추가

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -8,7 +8,7 @@ export default function Btns() {
 	const wrap = useRef(null);
 	const secs = useRef(null);
 	const btns = useRef(null);
-	const baseLine = useRef(-window.innerHeight / 2); //현재 섹션의 컨텐츠가 절반이상 보여야지 활성화처리
+	const baseLine = useRef(-window.innerHeight / 2);
 
 	const activation = () => {
 		const scroll = wrap.current.scrollTop;
@@ -20,8 +20,30 @@ export default function Btns() {
 			}
 		});
 	};
-	const handleClick = idx => {
-		new Anime(wrap.current, { scroll: secs.current[idx].offsetTop - 100 }, { duration: 1000 });
+
+	const moveScroll = idx => {
+		idx === 0 && new Anime(wrap.current, { scroll: 0 }, { duration: 500 });
+		if (idx) {
+			new Anime(wrap.current, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
+		}
+	};
+
+	const autoScroll = e => {
+		const btnsArr = Array.from(btns.current.children);
+		const activeEl = btns.current.querySelector('li.on');
+		//현재 활성화된 버튼의 순번구함
+		const activeIndex = btnsArr.indexOf(activeEl);
+
+		//휠 다운시
+		if (e.deltaY > 0) {
+			//현재순번이 마지막순번이 아니면 다음순번 섹션위치로 모션이동
+			activeIndex !== Num - 1 && moveScroll(activeIndex + 1);
+		} else {
+			//휠 업시
+			//현재순번이 첫번째순번이 아니면 이전순번 섹션 위치로 모션이동
+			activeIndex !== 0 && moveScroll(activeIndex - 1);
+			activeIndex === 0 && moveScroll(0);
+		}
 	};
 
 	const throttledActivation = useThrottle(activation);
@@ -31,8 +53,12 @@ export default function Btns() {
 		secs.current = wrap.current.querySelectorAll('.myScroll');
 		setNum(secs.current.length);
 
+		wrap.current.addEventListener('mousewheel', autoScroll);
 		wrap.current.addEventListener('scroll', throttledActivation);
-		return () => wrap.current.removeEventListener('scroll', throttledActivation);
+		return () => {
+			wrap.current.removeEventListener('scroll', throttledActivation);
+			wrap.current.removeEventListener('mousewheel', autoScroll);
+		};
 	}, [throttledActivation]);
 
 	return (
@@ -40,7 +66,7 @@ export default function Btns() {
 			{Array(Num)
 				.fill()
 				.map((_, idx) => {
-					return <li key={idx} className={idx === 0 ? 'on' : ''} onClick={() => handleClick(idx)}></li>;
+					return <li key={idx} className={idx === 0 ? 'on' : ''} onClick={() => moveScroll(idx)}></li>;
 				})}
 		</ul>
 	);
