b7e6bcb52f7cab0f656da5edfc226cdddd16ac3b
aajy <aajy.000@gmail.com>
2024-01-04 12:06:36 +0900

    Btns 스크롤 시 버튼 자동 활성화처리

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,22 +1,40 @@
-import { useEffect, useRef, useState } from 'react';
 import './Btns.scss';
+import { useRef, useState, useEffect } from 'react';
+
+//window.scrollY : 브라우저를 스크롤할때마다 스크롤되고 있는 거리값 (동적)
+//DOM.scrollTop : DOM요소안쪽에서 스크롤할때마다 스크롤되는 거리값 (동적)
+//DOM.offsetTop : 문서에서 해당 돔요소의 세로 위치값 (정적)
 
 export default function Btns() {
 	const [Index, setIndex] = useState(0);
-	const num = useRef(4);
+	const [Num, setNum] = useState(0);
+	const wrap = useRef(null);
+	const secs = useRef(null);
+	const btns = useRef(null);
+	const activation = () => {
+		const scroll = wrap.current.scrollTop;
 
+		secs.current.forEach((sec, idx) => {
+			if (scroll >= secs.current[idx].offsetTop) {
+				Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
+				btns.current.children[idx].classList.add('on');
+			}
+		});
+	};
 	useEffect(() => {
-		num.current = document.body.querySelectorAll('.myScroll').length;
-		console.log(document.body.querySelectorAll('.myScroll'));
-		console.log('num.current: ', num.current);
+		wrap.current = document.querySelector('.wrap');
+		secs.current = document.querySelectorAll('.myScroll');
+		setNum(secs.current.length);
+
+		wrap.current.addEventListener('scroll', activation);
 	}, []);
 
 	return (
-		<ul className='Btns'>
-			{Array(num.current)
+		<ul className='Btns' ref={btns}>
+			{Array(Num)
 				.fill()
 				.map((_, idx) => {
-					return <li key={idx} className={idx === Index ? 'on' : ''}></li>;
+					return <li key={idx} className={idx === Index ? 'on' : ''} onClick={() => setIndex(idx)}></li>;
 				})}
 		</ul>
 	);
