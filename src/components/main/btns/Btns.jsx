20fa5c5ab952a6a64de72656bd0670a4aaed7d81
aajy <aajy.000@gmail.com>
2024-01-04 13:17:09 +0900

    activation 함수 throttle적용. btn왼쪽으로 수정,

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,9 +1,7 @@
+import Anime from '../../../asset/anime';
 import './Btns.scss';
 import { useRef, useState, useEffect } from 'react';
-
-//window.scrollY : 브라우저를 스크롤할때마다 스크롤되고 있는 거리값 (동적)
-//DOM.scrollTop : DOM요소안쪽에서 스크롤할때마다 스크롤되는 거리값 (동적)
-//DOM.offsetTop : 문서에서 해당 돔요소의 세로 위치값 (정적)
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function Btns() {
 	const [Index, setIndex] = useState(0);
@@ -11,6 +9,7 @@ export default function Btns() {
 	const wrap = useRef(null);
 	const secs = useRef(null);
 	const btns = useRef(null);
+
 	const activation = () => {
 		const scroll = wrap.current.scrollTop;
 
@@ -21,20 +20,32 @@ export default function Btns() {
 			}
 		});
 	};
+	const throttledActivation = useThrottle(activation);
+
 	useEffect(() => {
 		wrap.current = document.querySelector('.wrap');
 		secs.current = document.querySelectorAll('.myScroll');
 		setNum(secs.current.length);
 
-		wrap.current.addEventListener('scroll', activation);
-	}, []);
+		wrap.current.addEventListener('scroll', throttledActivation);
+		return () => wrap.current.removeEventListener('scroll', throttledActivation);
+	}, [throttledActivation]);
 
 	return (
 		<ul className='Btns' ref={btns}>
 			{Array(Num)
 				.fill()
 				.map((_, idx) => {
-					return <li key={idx} className={idx === Index ? 'on' : ''} onClick={() => setIndex(idx)}></li>;
+					return (
+						<li
+							key={idx}
+							className={idx === Index ? 'on' : ''}
+							onClick={() => {
+								//new Anime(선택자, {속성명1:속성값2, 속성명2:속성값2}, {duration:속도, easeType:가속도, callback:컴플릭함수})
+								new Anime(wrap.current, { scroll: secs.current[idx].offsetTop - 100 }, { duration: 1000 });
+							}}
+						></li>
+					);
 				})}
 		</ul>
 	);
