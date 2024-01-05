9ac4a530291e1b113fa0a10a331ad5c7f8c42889
aajy <aajy.000@gmail.com>
2024-01-05 18:01:25 +0900

    Pics scroll값 이용해서직접 두개의 제목 style값 제어

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -5,13 +5,18 @@ import './Pics.scss';
 export default function Pics() {
 	const [Frame, setFrame] = useState(null);
 	const thisEl = useRef(null);
-	const boxEl = useRef(null);
+	const titEl = useRef(null);
+	const titEl2 = useRef(null);
 	const { getCurrentScroll } = useScroll(Frame);
 
 	const handleScroll = useCallback(() => {
 		const scroll = getCurrentScroll(thisEl.current, -window.innerHeight / 2);
 
-		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
+		if (scroll >= 0) {
+			titEl.current.style.transform = `translateX(${scroll}px)`;
+			titEl.current.style.opacity = 1 - scroll / 500;
+			titEl2.current.style.transform = `scale(${1 + scroll / 400}) translateX(${scroll * 0.5}px)`;
+		}
 	}, [getCurrentScroll]);
 
 	useEffect(() => {
@@ -25,7 +30,12 @@ export default function Pics() {
 
 	return (
 		<section className='Pics myScroll' ref={thisEl}>
-			<div className='box' ref={boxEl}></div>
+			<h3 className='tit' ref={titEl}>
+				FLICKR
+			</h3>
+			<h4 className='tit2' ref={titEl2}>
+				PREVIEW
+			</h4>
 		</section>
 	);
 }
