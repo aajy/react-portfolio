d8cde0edf5dc57e4bb6ab248a2a4cf0136dba91a
aajy <aajy.000@gmail.com>
2024-01-05 15:38:30 +0900

    Layout.jsx에 top버튼 스크롤 300 이상시에만 보이도록 useScroll 훅사용해서 적용

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,24 +1,42 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import './Layout.scss';
 import { useSplitText } from '../../../hooks/useText';
+import { useScroll } from '../../../hooks/useScroll';
 
-export default function Layout({ title, children }) {
+export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
 	const refTitle = useRef(null);
+	const refBtnTop = useRef(null);
 	const splitText = useSplitText();
+	const { scrollTo, getCurrentScroll, scrollFrame } = useScroll();
+
+	const handleScroll = useCallback(
+		num => {
+			getCurrentScroll() >= num ? refBtnTop.current?.classList.add('on') : refBtnTop.current?.classList.remove('on');
+		},
+		[getCurrentScroll]
+	);
 
 	useEffect(() => {
+		scrollTo(0);
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
-			refFrame.current && refFrame.current?.classList.add('on');
+			refFrame.current?.classList.add('on');
 		}, 300);
-	}, [splitText, title]);
+	}, [splitText, title, scrollTo]);
+
+	useEffect(() => {
+		scrollFrame?.addEventListener('scroll', () => handleScroll(300));
+	}, [getCurrentScroll, handleScroll, scrollFrame]);
+
 	return (
-		<main className={`Layout ${title}`} ref={refFrame}>
+		<main ref={refFrame} className={`Layout ${title}`}>
 			<h1 ref={refTitle}>{title}</h1>
-			<div className='btnTop'>Top</div>
 			<div className='bar'></div>
 			{children}
+			<button ref={refBtnTop} className='btnTop' onClick={() => scrollTo(0)}>
+				Top
+			</button>
 		</main>
 	);
 }
