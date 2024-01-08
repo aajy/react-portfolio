0cd0b9f0a17ff98b77d89e30af6fa2d010caaaed
aajy <aajy.000@gmail.com>
2024-01-08 10:08:12 +0900

    Banner.jsx 스크롤 모션 코드 추가

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
 
 export default function Banner() {
-	return <div className='Banner myScroll'>Banner</div>;
+	const [Frame, setFrame] = useState(null);
+	const refBanner = useRef(null);
+	const boxEl = useRef(null);
+
+	const { getCurrentScroll } = useScroll(Frame);
+
+	const handleScroll = useCallback(() => {
+		const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+		if (scroll >= 0) {
+			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
+			boxEl.current.style.opacity = 1 - scroll / 400;
+		}
+	}, [getCurrentScroll]);
+
+	useEffect(() => {
+		setFrame(refBanner.current?.closest('.wrap'));
+	}, []);
+
+	useEffect(() => {
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
+
+	return (
+		<section className='Banner myScroll' ref={refBanner}>
+			<div className='box' ref={boxEl}></div>
+		</section>
+	);
 }
